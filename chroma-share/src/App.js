747c3b059bfe8981495f53cc93e22aefa54ac331
Simone Zamparini <simone@alensxr.com>
2021-11-25 09:05:49 -0800

    Init ChromaShare React App

---
+++ chroma-share/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Content">
+        <h1>Chroma Share</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
