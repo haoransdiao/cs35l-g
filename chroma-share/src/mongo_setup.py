09a343e55c35219586998527e2f7a6b3758b7ba1
Elissa <esoykin@gmail.com>
2021-11-30 14:26:22 -0800

    create DB, connect to DB, add classes
    The ChromaShare database was created on MongoDB.
    The source code now connects to this database.
    The classes.py file includes classes to represent user accounts
    and photo posts, with the associated data for each type of object.
    Using mongoengine, the classes are mongoengine documents. Their data
    is mongoengine fields. They also have meta data to connect to the
    database.

---
+++ chroma-share/src/mongo_setup.py
@@ -0,0 +1,52 @@
+'''
+import mongoengine
+
+def global_init():
+    mongoengine.register_connection()
+
+
+
+from mongoengine import connect
+
+
+from pymongo import MongoClient
+'''
+
+
+
+'''
+import mongoengine
+
+
+alias_core = 'core'
+db = 'chromashare-db'
+
+data = dict(
+    alias=DEFAULT_CONNECTION_NAME,
+    name=db_name,
+    host=db_host,
+    port=db_port,
+    username=db_user,
+    password=db_pass,
+    read_preference=ReadPreference.NEAREST,
+    authentication_source=db_name,
+
+)
+
+def global_init():
+    mongoengine.register_connection()
+'''
+
+
+import mongoengine
+
+
+from mongoengine import connect
+#connect(host="mongodb+srv://chromashare.asmjs.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority")
+# ^ the URI from the chromashare mongoDB site
+
+
+
+def global_init():
+    connect(host="mongodb+srv://chromashare.asmjs.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority")
+    # ^ the URI from the chromashare mongoDB site
